b581a41ebb6fe2853746abd5a4be9d588d08d6ad
Daniel Byers <danielbyers0@gmail.com>
2023-01-30 13:58:51 -0500

    Idea Board

--- car_noise_cancellation_thought.py
+++ car_noise_cancellation_thought.py
@@ -1,1 +1,10 @@
 # start of car noise cancellation repo
+
+# ! Idea Board:
+
+# * 1. Make small module with mic, speaker and inverting op-amp
+    # This will read in mic audio and emit inverted audio to cancel
+    # Learn feedback anc theory
+
+# * 2. Replace modular speaker with Car Speakers
+    # Wire in inverting op-amp to car speakers for use
