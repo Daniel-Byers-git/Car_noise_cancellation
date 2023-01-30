124040d81f58232bb6ac2d27e1e33c1422f7333b
Daniel Byers <danielbyers0@gmail.com>
2023-01-30 14:07:22 -0500

    Addition to idea board

--- car_noise_cancellation_thought.py
+++ car_noise_cancellation_thought.py
@@ -6,5 +6,11 @@
     # This will read in mic audio and emit inverted audio to cancel
     # Learn feedback anc theory
 
-# * 2. Replace modular speaker with Car Speakers
+# * 2. Feed audio to arduino to detect monotonous long standing frequencies
+    # This will then modifiy an electrical component in the hardware ciruit to actively focus on emiiting those frequencies.
+    # Learn what this circuit aspect will be. That filters frequencies. 
+
+# * 3. Use op-amp in series with electrical filter 
+
+# * 4. Replace modular speaker with Car Speakers
     # Wire in inverting op-amp to car speakers for use
